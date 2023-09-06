d574187a5d06404ae0f2267ba6633e2889817dd9
Bigyuun <bigyun9375@gmail.com>
2023-09-06 13:04:06 +0900

    launch description update

--- src/loadcell_pkg/setup.py
+++ src/loadcell_pkg/setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         # ('share/' + package_name + '/launch', glob.glob(os.path.join('launch', '*launch.py'))),
-        (os.path.join('share', package_name, 'launch'), glob.glob(os.path.join('launch', 'launch.[pxy][yma]*')))
+        (os.path.join('share', package_name, 'launch'), glob.glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
