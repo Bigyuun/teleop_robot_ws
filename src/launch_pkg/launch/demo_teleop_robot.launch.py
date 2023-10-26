f71fbc1917c14740f86f0451910a19a2eb92d739
Bigyuun <bigyun9375@gmail.com>
2023-10-26 19:24:37 +0900

    rviz defaul_env update

--- src/launch_pkg/launch/demo_teleop_robot.launch.py
+++ src/launch_pkg/launch/demo_teleop_robot.launch.py
@@ -54,6 +54,7 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        output='screen'
+        output='screen',
+        parameters=[{'rviz_config': './rviz_env.rviz'}]
       ),
   ])
